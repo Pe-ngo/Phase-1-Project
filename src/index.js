421de26270fe348ddc0cb1ce22459ce7fae78ce4
Pe-ngo <jamespengo95@gmail.com>
2022-06-26 07:21:16 +0300

    Identify elements by id and get elements by query selector

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//Get the elements
+const buttonOne = document.querySelector('#buttonOne');
+const type = document.querySelector('#type');
+const typeOne = document.querySelector('#typeOne');
+const buttonTwo = document.querySelector('#buttonTwo');
+const openForm = document.querySelector('#openForm');
+const companyBrand = document.querySelector('#companyBrand');
+const logoImage = document.querySelector('#logo-image');
+const jobTitle = document.querySelector('#job-title');
+const locate = document.querySelector('#workLocation');
+const description = document.querySelector('#description');
+const dateOne = document.querySelector('#pub-date');
+
