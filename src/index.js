58130be7ebd606988c41f1d5619aa6f4663ccc7d
Pe-ngo <jamespengo95@gmail.com>
2022-06-26 15:16:57 +0300

    Add showCompanySection function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//Dom content loaded event
+document.addEventListener('DOMContentLoaded', () => {
+
 //Get the elements
 const buttonOne = document.querySelector('#buttonOne');
 const type = document.querySelector('#type');
@@ -11,9 +14,54 @@ const locate = document.querySelector('#workLocation');
 const description = document.querySelector('#description');
 const dateOne = document.querySelector('#pub-date');
 
+})
+
 //event listeners
 buttonOne.addEventListener('click', showCompanySection);
-type.addEventListener('select', selectCompanyName);
-buttonTwo.addEventListener('submit', displayJobDetails);
+type.addEventListener('change', selectCompanyName);
+// buttonTwo.addEventListener('submit', displayJobDetails);
 
 
+//functions
+function showCompanySection() {
+    type.value = 'Keeper Solutions'||'Keeper Solutions'||'ItsaCheckmate'||'Raidboxes'||'Teamwork';
+}
+
+function selectCompanyName() {
+    let displaytext = type.options[type.selectedIndex].text;
+    typeOne.value = displaytext;
+}
+
+
+//         logoImage.src = 'images/itsacheckmate.png';
+//         companyBrand.innerHTML = 'ItsaCheckmate';
+//     } else if (type.value === 'CodingAcademy') {
+//         typeOne.innerHTML = 'Coding Academy';
+//         logoImage.src = 'images/codingacademy.png';
+//         companyBrand.innerHTML = 'Coding Academy';
+//     } else if (type.value === 'Codecademy') {
+//         typeOne.innerHTML = 'Codecademy';
+//         logoImage.src = 'images/codecademy.png';
+//         companyBrand.innerHTML = 'Codecademy';
+//     } else if (type.value === 'Codecademy') {
+//         typeOne.innerHTML = 'Codecademy';
+//         logoImage.src = 'images/codecademy.png';
+//         companyBrand.innerHTML = 'Codecademy';
+//     } else if (type.value === 'Codecademy') {
+//         typeOne.innerHTML = 'Codecademy';
+//         logoImage.src = 'images/codecademy.png';
+//         companyBrand.innerHTML = 'Codecademy';
+//     } else if (type.value === 'Codecademy') {
+//         typeOne.innerHTML = 'Codecademy';
+//         logoImage.src = 'images/codecademy.png';
+//         companyBrand.innerHTML = 'Codecademy';
+//     } else if (type.value === 'Codecademy') {
+//         typeOne.innerHTML = 'Codecademy';
+//         logoImage.src = 'images/codecademy.png';
+//         companyBrand.innerHTML = 'Codecademy';
+//     } else if (type.value === 'Codecademy') {
+//         typeOne.innerHTML = 'Codecademy';
+//         logoImage.src = 'images/codecademy.png';
+//         companyBrand.innerHTML = 'Codecademy';
+//     }
+// }
